f948ce544b22cde3c87bcb62625df14b0ece8d19
user122904 <127532980+user122904@users.noreply.github.com>
2023-11-28 01:36:30 +0800

    Add project files.

---
+++ mysqlConnection/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using mysqlConnection.myclass;
+
+namespace mysqlConnection
+{
+    public partial class Form1 : Form
+    {
+        connection_class con = new connection_class();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.connectdb.Open();
+                label1.Text = "You are Connected to MySql Database!";
+                label1.ForeColor = Color.Green;
+                con.connectdb.Close();
+            }
+            catch (Exception)
+            {
+
+                label1.Text = "Failed to Connect to MySql Database!";
+                label1.ForeColor = Color.Red;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
